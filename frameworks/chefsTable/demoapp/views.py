37703d8179b2260f1ddb599a18621e02226364b5
Rodia <alejandro.ayala@onecluster.org>
2024-07-25 10:24:13 -0500

    Feat: Added a new urls and view home

--- frameworks/chefsTable/demoapp/views.py
+++ frameworks/chefsTable/demoapp/views.py
@@ -1,5 +1,9 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, 'demoapp/home.html')
 
 
 def index(request):
