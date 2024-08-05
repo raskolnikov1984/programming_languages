6aead4bda4b57b50f91054ab0c7e33dfa4b3db5e
sinergia <alejandro.ayala@onecluster.org>
2024-08-04 20:55:43 -0500

    Feat: New KnowLedge

--- frameworks/chefsTable/demoapp/urls.py
+++ frameworks/chefsTable/demoapp/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'demoapp'
 urlpatterns = [
     path('dishes/<str:dish>', views.menuitems),  # dish=pasta
     path('home', views.home, name='home'),
