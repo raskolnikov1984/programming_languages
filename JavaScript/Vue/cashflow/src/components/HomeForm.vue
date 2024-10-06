f2a6eec47deb521a71e5bec1afebf20e20822336
sinergia <alejandro.ayala@onecluster.org>
2024-10-05 21:14:33 -0500

    Feat: Layout, Header

--- JavaScript/Vue/cashflow/src/components/HomeForm.vue
+++ JavaScript/Vue/cashflow/src/components/HomeForm.vue
@@ -1,9 +1,21 @@
 <template>
-  <p>Hola Mundo</p>
+  <Layout>
+    <template #header>
+      <Header></Header>
+    </template>
+  </Layout>
 </template>
 
 <script>
+  import Layout from "./LayoutForm.vue";
+  import Header from "./HeaderForm.vue";
+
   export default {
     name: 'HomeForm',
+      components: {
+          Layout,
+          Header,
+      }
+
   }
 </script>
