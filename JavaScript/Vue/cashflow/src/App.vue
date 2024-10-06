343296a223ce3069add2efdf053595cac292ad87
sinergia <alejandro.ayala@onecluster.org>
2024-10-05 20:03:44 -0500

    Feat: CashFlow Project Vue

---
+++ JavaScript/Vue/cashflow/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <HomeForm />
+</template>
+
+<script>
+import HomeForm from "@/components/HomeForm.vue";
+
+export default {
+  name: 'App',
+  components: {
+      HomeForm,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ JavaScript/Vue/cashflow/src/components/HomeForm.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>Hola Mundo</p>
+</template>
+
+<script>
+  export default {
+    name: 'HomeForm',
+  }
+</script>
