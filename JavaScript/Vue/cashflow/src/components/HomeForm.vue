331763c9fb27a178fb61462c43b409b592f57c07
sinergia <alejandro.ayala@onecluster.org>
2024-10-12 10:11:09 -0500

    Feat: Movements

--- JavaScript/Vue/cashflow/src/components/HomeForm.vue
+++ JavaScript/Vue/cashflow/src/components/HomeForm.vue
@@ -3,19 +3,28 @@
     <template #header>
       <Header></Header>
     </template>
+    <template #resume>
+      <Resume></Resume>
+    </template>
+    <template #movements>
+      <Movements></Movements>
+    </template>
   </Layout>
 </template>
 
 <script>
-  import Layout from "./LayoutForm.vue";
-  import Header from "./HeaderForm.vue";
+ import Layout from "./LayoutForm.vue";
+ import Header from "./HeaderForm.vue";
+ import Resume from "./Resume/IndexForm.vue";
+ import Movements from "./MovementsForm.vue";
 
-  export default {
-    name: 'HomeForm',
-      components: {
-          Layout,
-          Header,
-      }
-
-  }
+ export default {
+   name: 'HomeForm',
+   components: {
+     Layout,
+     Header,
+     Resume,
+     Movements
+   }
+ }
 </script>
