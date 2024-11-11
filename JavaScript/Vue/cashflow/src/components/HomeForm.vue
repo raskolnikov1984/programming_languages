4c895467501345e48817809fbeff4497a6ce78b8
sinergia <alejandro.ayala@onecluster.org>
2024-11-10 22:36:59 -0500

    Feat: Movimientos de los ultimos 30 Dias

--- JavaScript/Vue/cashflow/src/components/HomeForm.vue
+++ JavaScript/Vue/cashflow/src/components/HomeForm.vue
@@ -31,27 +31,135 @@
  import Graphic from "./Resume/GraphicView.vue";
 
  export default {
-   name: 'HomeForm',
-   components: {
-       Layout,
-       Header,
-       Resume,
-       Action,
-       Movements,
-       Graphic
-   },
+     name: 'HomeForm',
+     components: {
+         Layout,
+         Header,
+         Resume,
+         Action,
+         Movements,
+         Graphic
+     },
      data(){
          return {
              amount: null,
              label: null,
-             amounts: [100, 200, 500, 200, -400, -600, -300, 0, 300, 500],
+             // amounts: [100, 200, 500, 200, -400, -600, -300, 0, 300, 500],
              movements: [
-                 {id: 0, title: "Movimiento1",
-                  description: "Description", amount: 1000},
-                 {id: 1, title: "Movimiento2", description: "Description", amount: 2000},
-                 {id: 2, title: "Movimiento3", description: "Description", amount: -3000}
+                 {
+                     id: 0,
+                     title: "Movimiento 1",
+                     description: "Lorem ipsum dolor sit amet",
+                     amount: 100,
+                     time: new Date("20-10-2024"),
+                 },
+                 {
+                     id: 1,
+                     title: "Movimiento 2",
+                     description: "Lorem ipsum dolor sit amet",
+                     amount: 200,
+                     time: new Date("02-01-2024"),
+                 },
+                 {
+                     id: 2,
+                     title: "Movimiento 3",
+                     description: "Lorem ipsum dolor sit amet",
+                     amount: 500,
+                     time: new Date("02-01-2024"),
+                 },
+                 {
+                     id: 3,
+                     title: "Movimiento 4",
+                     description: "Lorem ipsum dolor sit amet",
+                     amount: 200,
+                     time: new Date("02-01-2024"),
+                 },
+                 {
+                     id: 4,
+                     title: "Movimiento 5",
+                     description: "Lorem ipsum dolor sit amet",
+                     amount: -400,
+                     time: new Date("02-01-2024"),
+                 },
+                 {
+                     id: 5,
+                     title: "Movimiento 6",
+                     description: "Lorem ipsum dolor sit amet",
+                     amount: -600,
+                     time: new Date("02-01-2024"),
+                 },
+                 {
+                     id: 6,
+                     title: "Movimiento 7",
+                     description: "Lorem ipsum dolor sit amet",
+                     amount: -300,
+                     time: new Date("02-01-2024"),
+                 },
+                 {
+                     id: 7,
+                     title: "Movimiento 8",
+                     description: "Lorem ipsum dolor sit amet",
+                     amount: 100,
+                     time: new Date("10-20-2024"),
+                 },
+                 {
+                     id: 8,
+                     title: "Movimiento 9",
+                     description: "Lorem ipsum dolor sit amet",
+                     amount: -300,
+                     time: new Date("10-25-2024"),
+                 },
+                 {
+                     id: 9,
+                     title: "Movimiento 10",
+                     description: "Lorem ipsum dolor sit amet",
+                     amount: 500,
+                     time: new Date("10-28-2024"),
+                 },
+                 {
+                     id: 10,
+                     title: "Movimiento 11",
+                     description: "Lorem ipsum dolor sit amet",
+                     amount: 500,
+                     time: new Date("10-29-2024"),
+                 },
+                 {
+                     id: 11,
+                     title: "Movimiento 11",
+                     description: "Lorem ipsum dolor sit amet",
+                     amount: 500,
+                     time: new Date("10-30-2024"),
+                 },
+                 {
+                     id: 12,
+                     title: "Movimiento 11",
+                     description: "Lorem ipsum dolor sit amet",
+                     amount: 500,
+                     time: new Date("10-31-2024"),
+                 },
              ]
          }
      },
+     computed: {
+         amounts() {
+             const lastDays = this.movements
+                                  .filter(m => {
+                                      const today = new Date();
+                                      const oldDate = today.setDate(today.getDate() - 30);
+
+                                      return m.time > oldDate;
+                                  })
+                                  .map(m => m.amount);
+
+             return lastDays.map((m, i) => {
+                 const lastMovements = lastDays.slice(0, i);
+
+                 return lastMovements.reduce((suma, movement) => {
+                     return suma + movement
+                 }, 0);
+             });
+         },
+     },
  }
+
 </script>
